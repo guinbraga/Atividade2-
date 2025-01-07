83d8f8f6ef64829382b8c1fcd2297254c7d373dd
guinbraga <guilhormo.47@gmail.com>
2025-01-07 17:53:58 -0300

    Realizado o ultimo exercicio da atividade

---
+++ Fila com Nó-Cabeça/mainFila.c
@@ -0,0 +1,19 @@
+#include "fila_dinamica.c"
+
+int main(){
+    printf("Inicializando fila:\n");
+    FILA fila;
+    inicializarFila(&fila);
+    printf("Adicionando 3 elementos:\n");
+    REGISTRO adicionar;
+    for (int i = 0; i < 3; i++){
+        printf("\nDigite o %dº numero a ser adicionado: ", i+1);
+        scanf("%d", &adicionar.chave);
+        inserirNaFila(&fila, adicionar);
+    }
+    exibirFila(&fila);
+    printf("\nExcluindo um elemento.\n");
+    REGISTRO excluido;
+    excluirDaFila(&fila, &excluido);
+    exibirFila(&fila);
+}
